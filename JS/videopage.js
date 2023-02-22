7b76a284beca5bc8794ec667e15f8d7b1764dfd0
emmaelisabetheide <Ehovikeide@gmail.com>
2023-02-22 12:42:57 +0100

    Add video nav and videos

---
+++ JS/videopage.js
@@ -0,0 +1,12 @@
+"use strict";
+//
+// ***** KNITTING VIDEOES PAGE ***** //
+//
+
+// var obj = {
+//   video: {
+//     value:
+//       "<iframe title='YouTube video player' type=\"text/html\" width='640' height='390' src='https://www.youtube.com/watch?v=itPw3_F8oFU&list=PLwlHzAqV2HULaR0617ZS1kWJWhcIq4BiT&index' frameborder='0' allowFullScreen></iframe>",
+//   },
+// };
+// document.write(obj.video.value);
